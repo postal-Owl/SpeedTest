f02ee26326954ae774bea60e0073de7b6276c8f3
postal-Owl <108006845+postal-Owl@users.noreply.github.com>
2026-03-15 05:10:48 +0700

    Файлы для сборки проекта
    На сколько я понял этого достаточно, чтобы при желании самостоятельно собрать проект

---
+++ Build/SpeedTest.cpp
@@ -0,0 +1,11 @@
+﻿// SpeedTest.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "SpeedTest.h"
+
+template<typename... Tasks>
+void SpeedTest(const int& N, Tasks... tasks);
+
+//  /\__/\
+// (=^x^=)
+//  (")(")_/
