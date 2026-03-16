940fe4bacb5b102155d396f4e7bd9f1dfb331c14
postal-Owl <108006845+postal-Owl@users.noreply.github.com>
2026-03-17 03:28:20 +0700

    Patch v1.1

--- SpeedTest.h
+++ SpeedTest.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 #ifndef SPEEDTEST_H
 #define SPEEDTEST_H
@@ -11,9 +12,11 @@
 // [] { foo(); },
 // Кол-во передаваемых лямбда функций - неограничено
 template<typename... Tasks>
-void SpeedTest(const int& N, Tasks... tasks) {
-    uint8_t count = 0;
-    auto start_total = std::chrono::high_resolution_clock::now();
+std::vector<double> SpeedTest(const int& N, Tasks... tasks) {
+    std::vector<double> Time;
+
+    // unsigned int count = 0;
+    // os << "Блоков проверено: " << count;
 
     ([&] {
         double total = 0;
@@ -23,20 +26,60 @@ void SpeedTest(const int& N, Tasks... tasks) {
             auto end = std::chrono::high_resolution_clock::now();
             total += std::chrono::duration<double>(end - start).count();
         }
-        std::cout << "Среднее время работы блока " << static_cast<int>(count) << ": " << total / N << " сек." << std::endl;
-        count++;
-        }(), ...);
+        Time.push_back(total / N);
+        // count++;
+        // for (int i = 0; pow(10, i) < count; i++)
+        //     os << '\b';
+        // os << count;
 
-    auto end_total = std::chrono::high_resolution_clock::now();
-    std::cout << "Общее время выполнения: " << std::chrono::duration<double>(end_total - start_total).count() << " сек." << std::endl;
+        // count++;
+        // os.seekp(18);
+        // os << count;
+        }(), ...);
+    // os << '\n';
+
+    return Time;
 }
 
+template<typename SubTask, typename... Tasks>
+std::vector<double> SpeedTestSub(const int& N, SubTask First, Tasks... tasks) {
+    std::vector<double> Time;
+
+    double SubTime = 0;     // Предполагается, что этот блок присутствует во всех прочих блоках
+    for (int i = 0; i < N; i++) {
+        auto start = std::chrono::high_resolution_clock::now();
+        First();
+        auto end = std::chrono::high_resolution_clock::now();
+        SubTime += std::chrono::duration<double>(end - start).count();
+    }
+
+    SubTime /= N;
+    // unsigned int count = 0;
+    // os << "Блоков проверено: " << count;
+
+    ([&] {
+        double total = 0;
+        for (int i = 0; i < N; i++) {
+            auto start = std::chrono::high_resolution_clock::now();
+            tasks();
+            auto end = std::chrono::high_resolution_clock::now();
+            total += std::chrono::duration<double>(end - start).count();
+        }
+        Time.push_back(total / N - SubTime);
+        // count++;
+        // for (int i = 0; pow(10, i) < count; i++)
+        //     os << '\b';
+        // os << count;
+
+        // count++;
+        // os.seekp(18);
+        // os << count;
+        }(), ...);
+    // os << '\n';
+    
+    return Time;
+}
+
+void STPrint(std::vector<double> data, std::ostream& os);
+
 #endif // SPEEDTEST_H
-
-
-
-
-
-//  /\__/\
-// (=^x^=)
-//  (")(")_/
